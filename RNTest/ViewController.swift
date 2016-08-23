b8bbacf2c3e9a3909e7f31ce2ea067df85fe1f62
Emerson Malca <emersonmalca@me.com>
2016-08-23 16:13:27 -0700

    Added main js file and module

--- RNTest/ViewController.swift
+++ RNTest/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import React
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let jsCodeFilePath = NSBundle.mainBundle().URLForResource("index.ios", withExtension: "js")
+        
+        let mockData:NSDictionary = ["scores":
+            [
+                ["name":"Alex", "value":"42"],
+                ["name":"Joel", "value":"10"]
+            ]
+        ]
+        
+        let dynamicView = RCTRootView(bundleURL: jsCodeFilePath, moduleName:"RNHighScores", initialProperties:mockData, launchOptions:nil)
+        self.view.addSubview(dynamicView)
     }
 
     override func didReceiveMemoryWarning() {
